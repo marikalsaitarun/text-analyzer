ad83de8f3df52f9ed923212d583a994cee1d8154
marikalsaitarun <saitarunmarikal@gmail.com>
2026-08-12 17:57:46 +0530

    Update welcome message

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ age = input("How old are you? ")
 
 print(f"\nHello {name}!")
 print(f"You are {age} years old.")
-print("Welcome to your first GitHub project!")
+print("Welcome to Tarun's GitHub project!")
 
 
 future_age = int(age) + 5
 
 print(f"In 5 years, you will be {future_age} years old.")
+print("This change was made on GitHub!")
